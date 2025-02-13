a742c27960415d9265e8fb46963b24584a4c4dd3
ANNIE CROTEAU <anniecroteauc@gmail.com>
2025-02-13 11:35:15 +0100

    chore/ Installation de tailwindCSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,8 +15,9 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <p className="text-green font-bold font-title text-2xl">Hello world</p>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-blue font-bold font-body">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}

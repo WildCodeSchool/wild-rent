3875b582f143b244f22ad87e2b5217cc895167d1
Laetitia <laeptitchat29@gmail.com>
2025-02-14 12:35:13 +0100

    fix conflits

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,29 @@ import Layout from "./pages/layout";
 import Home from "./pages/Home";
 import ProductsByCategories from "./pages/ProductsByCategories";
 import ProductDetails from "./pages/ProductDetails";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
+import ConfirmEmailPage from "./pages/ConfirmEmail";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route
-          path="produits/categorie/:title"
-          element={<ProductsByCategories />}
-        />
-        <Route path="/produit/:id" element={<ProductDetails />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route
+            path="produits/categorie/:title"
+            element={<ProductsByCategories />}
+          />
+          <Route path="produit/:id" element={<ProductDetails />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="confirm/:code?" element={<ConfirmEmailPage />} />
+        </Route>
+      </Routes>
+      <ToastContainer theme="colored" />
+    </>
   );
 }
 

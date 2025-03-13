c3f164f72b81f24a8f1a58639793c1ecb4de50de
Laetitia <laeptitchat29@gmail.com>
2025-03-13 12:26:47 +0100

    ajout test

---
+++ e2e/tests/poductDetails.test.ts
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test";
+
+test("test click article and select dates", async ({ page }) => {
+  await page.goto("http://localhost:7000/produit/2");
+  //await page.pause();
+
+  await page.getByRole("textbox", { name: "Début" }).click();
+  await page
+    .getByRole("option", { name: "Choose Thursday, March 13th," })
+    .click();
+  await page.getByRole("textbox", { name: "Fin" }).click();
+  await page
+    .getByRole("option", { name: "Choose Friday, March 14th," })
+    .click();
+  await expect(page.getByText("Total: 3€")).toHaveText("Total: 3€");
+
+  await page.getByRole("textbox", { name: "Début" }).click();
+  await page.getByRole("textbox", { name: "Fin" }).click();
+  await page
+    .getByRole("option", { name: "Choose Friday, March 28th," })
+    .click();
+  await expect(page.getByText("Total: 45€")).toHaveText("Total: 45€");
+});
+
+test("add to basket", async ({ page }) => {
+  await page.goto("http://localhost:7000/produit/2");
+  await page.pause();
+});

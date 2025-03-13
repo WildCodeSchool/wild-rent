8464a8d2f1042c34537df0def087ec13dc952cbb
RomanBeldent <romanbeldent@gmail.com>
2025-03-13 16:54:01 +0100

    feature(#47): ajoute un utilisateur jon snow par défaut

--- backend/src/config/fixtures.ts
+++ backend/src/config/fixtures.ts
@@ -3,10 +3,21 @@ import { dataSource } from "../config/db";
 import * as argon2 from "argon2";
 import { User } from '../entities/User';
 
+
 async function createUsers() {
     await dataSource.initialize();
-
+    console.log("📡 Database connected!");
     const users = [];
+    
+    // Création d'un utilisateur par défaut
+    users.push(({
+        first_name: 'Jon',
+        last_name: 'Snow',
+        email: 'jonsnow@wild-rent.com',
+        hashed_password: await argon2.hash('password'),
+        phone_number: '0606060606',
+        created_at: new Date()
+    }));
 
     for (let i = 0; i < 5; i++) {
         const first_name = faker.person.firstName();
@@ -20,13 +31,14 @@ async function createUsers() {
             first_name,
             last_name,
             email,
-            hashed_password,
             phone_number,
+            hashed_password,
             created_at
         });
     }
 
     await User.save(users);
+    console.log("✅ Users created successfully!");
 }
 
 async function createCategories() {
@@ -34,9 +46,9 @@ async function createCategories() {
 
     for (let i = 0; i < 5; i++) {
         categories.push({
-            
+
         })
     }
 }
- 
+
 createUsers();

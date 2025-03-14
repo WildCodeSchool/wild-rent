615c2a3d6ebe8bb5f744fc89d03b4762035dd18a
halicksse <alix.carlhant.pro@gmail.com>
2025-03-14 14:24:42 +0100

    feat: création images pour le faker

---
+++ backend/src/assets/productImages.ts
@@ -0,0 +1,33 @@
+
+
+const productImages: Record<string, string> = {
+    "Ski": [],
+    "chaussures de ski": [],
+    "chaussures de snow": [],
+    "fixations de ski": [],
+    "fixations de snow": [],
+    "batons de ski": [],
+    "Snowboard femme": [],
+    "Snowboard homme": [],
+    "surfboard": [],
+    "bodyboard": [],
+    "combinaison de surf": [],
+    "palmes": [],
+    "bouée": [],
+    "stand up paddle": [],
+    "tente": [],
+    "sac de couchage": [],
+    "rechaud": [],
+    "bbq": ["camping/bbq.jpg"],
+    "vtt": [],
+    "vtt électrique": [],
+    "casque de vtt": [],
+    "gants de vtt": [],
+    "vélo de route": [],
+    "batons de marche": [],
+    "chaussures de marche femme": [],
+    "chaussures de marche homme": [],
+    "chaussures de marche enfant": []
+};
+
+export default productImages;

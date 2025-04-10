76faf40dc3dff62d92c720c8c28920800776a0de
ThomGateau <thom.gateau@gmail.com>
2025-04-10 12:13:31 +0200

    add textarea resizing

--- frontend/src/pages/AdminArticle.tsx
+++ frontend/src/pages/AdminArticle.tsx
@@ -1,11 +1,13 @@
-import { ArticleFrom } from "../components/ArticleForm";
+import { ArticleForm } from "../components/ArticleForm";
 
 export const AdminArticle = () => {
   return (
     <>
       <div>
-        <h2 className="font-bold text-3xl">Nouvel article</h2>
-        <ArticleFrom createOrUpdate="create" />
+        <h2 className="font-bold text-3xl mb-6">Nouvel article</h2>
+        <div className="space-y-4 w-lg">
+          <ArticleForm createOrUpdate="create" />
+        </div>
       </div>
     </>
   );

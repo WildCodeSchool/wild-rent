40b9e57c666b494586888e9428eccf05b3212adf
Laetitia <laeptitchat29@gmail.com>
2025-04-10 09:44:18 +0200

    test cart ok

--- e2e/tests/Cart.test.ts
+++ e2e/tests/Cart.test.ts
@@ -1,17 +1,28 @@
 import { test, expect } from "@playwright/test";
 
+const baseUrl = "http://localhost:7000";
+
+function formatDate(day: number): string {
+  const today = new Date();
+  const date = new Date(Date.UTC(today.getFullYear(), today.getMonth(), day));
+  const weekday = date.toLocaleDateString("en-US", { weekday: "long" });
+  const month = date.toLocaleDateString("en-US", { month: "long" });
+  const label = `Choose ${weekday}, ${month} ${day}th,`;
+  return label;
+}
+const date13 = formatDate(13);
+const date14 = formatDate(14);
+const date28 = formatDate(28);
+
 test("test", async ({ page }) => {
-  await page.goto("http://localhost:7000/produit/2");
+  await page.goto(baseUrl + "/produit/2");
 
   await page.getByRole("textbox", { name: "Début" }).click();
-  await page
-    .getByRole("option", { name: "Choose Wednesday, April 9th," })
-    .click();
+  await page.getByRole("option", { name: date13 }).click();
   await page.getByRole("textbox", { name: "Fin" }).click();
-  await page
-    .getByRole("option", { name: "Choose Thursday, April 10th," })
-    .click();
+  await page.getByRole("option", { name: date14 }).click();
   await page.getByRole("button", { name: "Ajouter au panier" }).click();
-
   await expect(page.getByText("Mon panier (1)")).toBeVisible();
+  await page.getByRole("button", { name: "Ajouter au panier" }).click();
+  await expect(page.getByText("Mon panier (2)")).toBeVisible();
 });

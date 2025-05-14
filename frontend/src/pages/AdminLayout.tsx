a7f4e0da1f12c8a1f9ba83bddc1f80458ddc93b2
ThomGateau <thom.gateau@gmail.com>
2025-05-14 16:03:13 +0200

    fix: css

--- frontend/src/pages/AdminLayout.tsx
+++ frontend/src/pages/AdminLayout.tsx
@@ -3,9 +3,9 @@ import { AdminNavbar } from "../components/AdminNavbar";
 
 export const AdminLayout = () => {
   return (
-    <div className="flex h-screen">
+    <div className="grid grid-cols-[auto_1fr] h-screen">
       <AdminNavbar />
-      <main className="flex-1 p-8 ">
+      <main className="overflow-auto p-4 bg-gray-50">
         <Outlet />
       </main>
     </div>

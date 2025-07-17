2b11b921136c0b6f06b7eab1bd950e5b5d8cd8c4
RomanBeldent <romanbeldent@gmail.com>
2025-07-17 09:27:57 +0200

    fix(#109): corrige le middle IsUser en utilisant args afin de récupérer l'utilisateur courant

--- backend/src/middleware/AuthChecker.ts
+++ backend/src/middleware/AuthChecker.ts
@@ -3,15 +3,15 @@ import { MiddlewareFn } from "type-graphql";
 import { GraphQLError } from "graphql";
 
 // Middleware TypeGraphQL : utilisé pour sécuriser l'accès à certains champs ou résolveurs.
-// Ici, `IsUser` permet de restreindre l'accès aux données personnelles (email, téléphone, adresse...) 
+// Ici, `IsUser` permet de restreindre l'accès aux données personnelles (email, téléphone, adresse...)
 // uniquement à l'utilisateur concerné ou à un administrateur.
 // root correspond à l'utilisateur actuellement connecté
 // Voir la doc pour plus d'infos sur les middlewares -> https://typegraphql.com/docs/middlewares.html
-export const IsUser: MiddlewareFn<AuthContextType> = async (
-  { context, root },
+export const IsCurrentUserOrAdmin: MiddlewareFn<AuthContextType> = async (
+  { context, args },
   next
 ) => {
-  if (context.user.role === "ADMIN" || context.user.id === root.id) {
+  if (context.user.role === "ADMIN" || context.user.id === args.data.userId) {
     return next(); // on exécute le code du resolver si la condition est remplie
   } else {
     throw new GraphQLError("You are not authorized to perform this action.", {
@@ -27,8 +27,8 @@ export const IsAdmin: MiddlewareFn<AuthContextType> = async (
   { context },
   next
 ) => {
-  if (context.user.role === "ADMIN") { 
-    return next(); 
+  if (context.user.role === "ADMIN") {
+    return next();
   }
   throw new GraphQLError("You are not authorized to perform this action.", {
     extensions: {

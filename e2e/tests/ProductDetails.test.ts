03a8aacd252cc1c8aa82cc94302142103bf34ed2
ANNIE CROTEAU <anniecroteauc@gmail.com>
2025-08-26 15:29:10 +0200

    fix: product details test

--- e2e/tests/ProductDetails.test.ts
+++ e2e/tests/ProductDetails.test.ts
@@ -2,7 +2,10 @@ import { test, expect } from "@playwright/test";
 
 test("test click article and add cart", async ({ page }) => {
   await page.goto("http://localhost:7000/produit/1");
-  await page.getByText("15€ / jour").click();
+  await page.getByText("15€ / jour");
+  await page
+    .getByRole("combobox")
+    .selectOption('{"id":1,"size":"150 cm","total_quantity":10}');
   await page.getByRole("button", { name: "Ajouter au panier" }).click();
   await expect(page.getByText("Mon panier (1)")).toBeVisible();
   await page.getByRole("button", { name: "Ajouter au panier" }).click();

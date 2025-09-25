1e09f808d494bef1d5a873d04d2fd83f0c00444e
ThomGateau <thom.gateau@gmail.com>
2025-09-25 11:52:48 +0200

    fix:test

--- e2e/tests/Cart.test.ts
+++ e2e/tests/Cart.test.ts
@@ -39,9 +39,6 @@ test("Panier : ajout produit, sélection dates, modification quantité, suppress
 
   await page.getByRole("button", { name: "Ajouter au panier" }).click();
 
-  await expect(
-    page.getByText("Vous avez ajouter un article à votre panier")
-  ).toBeVisible();
   await page.goto(`${baseUrl}/panier`);
 
   await expect(page.getByText("45€")).toBeVisible();

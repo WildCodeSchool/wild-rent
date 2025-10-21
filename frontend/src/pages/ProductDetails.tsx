3845b22b3dead12e9b508cb7f5408179f23b5792
ANNIE CROTEAU <anniecroteauc@gmail.com>
2025-10-21 12:42:19 +0200

    rebase staging in dev (keep dev changes)

--- frontend/src/pages/ProductDetails.tsx
+++ frontend/src/pages/ProductDetails.tsx
@@ -1,5 +1,5 @@
 import { useNavigate, useParams } from "react-router-dom";
-import { useNavigate, useParams } from "react-router-dom";
+
 import {
   useGetAvailableProductOptionsQuery,
   useGetProductByIdQuery,
